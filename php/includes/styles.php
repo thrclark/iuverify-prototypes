b18e1e960d5ebe5a933a4e3a142994b121dfd340
Tom Clark <thrclark@iu.edu>
2017-03-09 06:46:38 -0500

    Cas styling

--- php/includes/styles.php
+++ php/includes/styles.php
@@ -1,5 +1,5 @@
-<!-- Base CSS -->
-<link href="../css/styles.css"  type="text/css" rel="stylesheet">
+ <!-- Base CSS -->
+<link href="../css-cas/styles.css"  type="text/css" rel="stylesheet">
 <link href="../bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css"  type="text/css" rel="stylesheet">
 <link href="https://fonts.iu.edu/style.css?family=BentonSans:regular,bold|BentonSansCond:regular|GeorgiaPro:regular" media="screen" rel="stylesheet" type="text/css"/>
 <style type="text/css">
