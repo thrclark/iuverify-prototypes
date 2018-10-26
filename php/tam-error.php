7abaa9ef038157a41b7da607c3cca76801579c96
Tom Clark <thrclark@iu.edu>
2018-10-26 07:17:16 -0400

    Update php/tam-error.php

---
+++ php/tam-error.php
@@ -0,0 +1,41 @@
+<?php $page_title = "Home" ?>
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+<?php include ('includes/head-meta.php') ?>
+<?php include ('includes/styles.php') ?>
+<style>
+body, h1 {
+	font-family: sans-serif
+}
+.header {
+	background: #222222;
+	margin-bottom: 30px;
+}
+.header h1 {
+	color: #ebebeb;
+	margin-bottom: 28px;
+}
+</style>
+</head>
+<body>
+<div class="header">
+    <div class="container" role="main">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>Page Error</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="main">
+    <div class="container" role="main">
+        <div class="row">
+            <div class="col-sm-12 lead">
+                <div class=""> An unexpected error has occurred. If this error persists, please contact your system administrator.</div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
